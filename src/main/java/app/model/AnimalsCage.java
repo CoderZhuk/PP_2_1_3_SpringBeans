7ac18d14794aac231da3452e08ba6341305dfa66
Coder.Z <Coder.Z@yandex.ru>
2023-09-07 20:52:54 +0200

    innit commit

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,25 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
+    @Autowired
+    private Timer timer;
+    @Autowired
+    public AnimalsCage(Timer timer) {
+        this.timer = timer;
+    }
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
+
+    public AnimalsCage(Animal animal) {
+        this.animal = animal;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
@@ -15,5 +27,9 @@ public class AnimalsCage {
         System.out.println("At:");
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return timer;
     }
 }
